623666e9d4eadbf92ba8bb7ef17e0c0c1c088480
Viet Huong <tranviethuong30102005@gmail.com>
2025-03-02 08:34:37 +0700

    test nhánh 1

---
+++ baiatp27.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	int a, b, c; cin >> a >> b >> c;
+	if(a == 0){
+		if(b == 0 && c == 0){
+			cout << "inf";
+		}
+		else if(b == 0 && c != 0){
+			cout << "no";
+		}
+		else if(b != 0){
+			cout << fixed << setprecision(2) << (float)-c/b;
+		}
+	}
+	else{
+		int delta = b * b - 4 * a * c;
+		if(delta < 0){
+			cout << "no";
+		}
+		else if(delta == 0){
+			cout << fixed << setprecision(2) << (float)-b / (2 * a);
+		}
+		else{
+			float x1 = (-b + sqrt(delta)) / (2 * a);
+			float x2 = (-b - sqrt(delta)) / (2 * a);
+			cout << fixed << setprecision(2) << x1 << " " << x2 << endl;
+		}
+	}
+	return 0;
+}
